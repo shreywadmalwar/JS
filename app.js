a28dd30a1858e5ded18201fe132ba7ab382afe2a
Shreyash Wadmalwar <sdwadmalwar@gmail.com>
2023-05-02 22:04:56 +0530

    adding code to github on branch js_project

--- app.js
+++ app.js
@@ -1,45 +1,89 @@
-// select the form element and listen for the submit event
-const form = document.getElementById('product-form');
-form.addEventListener('submit', addProduct);
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("product-form");
+  form.addEventListener("submit", addProduct);
 
-// define the addProduct function
-function addProduct(event) {
-  // prevent the default form submission behavior
-  event.preventDefault();
+  function addProduct(event) {
+    event.preventDefault();
+  
+    const brand = document.getElementById("product-brand").value;
+    const name = document.getElementById("product-name").value;
+    const details = document.getElementById("product-details").value;
+    const image = document.getElementById("product-image").value;
+    const cost = document.getElementById("product-cost").value;
+    const ratings = document.getElementById("ratings").value;
+  
+    if (!brand || !name || !details || !image || !cost || !ratings) {
+      alert("Please fill in all required fields");
+      return;
+    }
+  
+    var brand_info = localStorage.setItem("brand", brand);
+    var name_info = localStorage.setItem("name", name);
+    var details_info = localStorage.setItem("details", details);
+    var image_info = localStorage.setItem("image", image);
+    var cost_info = localStorage.setItem("cost", cost);
+    var rating_info = localStorage.setItem("ratings", ratings);
+  
+    document.getElementById("product-form").reset();
 
-  // select the input fields and get their values
-  const brand = document.getElementById('product-brand').value;
-  const name = document.getElementById('product-name').value;
-  const details = document.getElementById('product-details').value;
-  const image = document.getElementById('product-image').value;
-  const cost = document.getElementById('product-cost').value;
-  const ratings = document.getElementById('ratings').value;
+  // rest of your code...
+  }
+});
 
-  // create a new list item element
-  const li = document.createElement('li');
 
-  // set the inner HTML of the list item element
-  li.innerHTML = `
-    <div class="product">
-      <div class="product-image">
-        <img src="${image}" alt="${name}" />
+// const form = document.getElementById("product-form");
+// form.addEventListener("submit", addProduct);
+
+// function addProduct(event) {
+//   event.preventDefault();
+
+//   const brand = document.getElementById("product-brand").value;
+//   const name = document.getElementById("product-name").value;
+//   const details = document.getElementById("product-details").value;
+//   const image = document.getElementById("product-image").value;
+//   const cost = document.getElementById("product-cost").value;
+//   const ratings = document.getElementById("ratings").value;
+
+//   if (!brand || !name || !details || !image || !cost || !ratings) {
+//     alert("Please fill in all required fields");
+//     return;
+//   }
+
+//   var brand_info = localStorage.setItem("brand", brand);
+//   var name_info = localStorage.setItem("name", name);
+//   var details_info = localStorage.setItem("details", details);
+//   var image_info = localStorage.setItem("image", image);
+//   var cost_info = localStorage.setItem("cost", cost);
+//   var rating_info = localStorage.setItem("ratings", ratings);
+
+//   document.getElementById("product-form").reset();
+// }
+
+function retrieveProduct() {
+  const brand = localStorage.getItem("brand");
+  const name = localStorage.getItem("name");
+  const details = localStorage.getItem("details");
+  const image = localStorage.getItem("image");
+  const cost = localStorage.getItem("cost");
+  const ratings = localStorage.getItem("ratings");
+
+  const productInfoElement = document.getElementById("product-info");
+
+  if (productInfoElement) {
+    const productHTML = `
+      <div>
+        <img src="${image}" alt="${name}">
+        <h2>${name}</h2>
+        <p>${details}</p>
+        <p>${brand}</p>
+        <p>$${cost}</p>
+        <p>Ratings: ${ratings}</p>
       </div>
-      <div class="product-info">
-        <h2 class="product-name">${name}</h2>
-        <p class="product-details">${details}</p>
-        <div class="product-meta">
-          <p class="product-brand">${brand}</p>
-          <p class="product-cost">$${cost}</p>
-          <p class="product-ratings">${ratings} stars</p>
-        </div>
-      </div>
-    </div>
-  `;
+    `;
 
-  // select the product list and append the new list item element to it
-  const productList = document.getElementById('product-list');
-  productList.appendChild(li);
-
-  // reset the form
-  form.reset();
+    productInfoElement.innerHTML = productHTML;
+  }
 }
+
+
+window.onload = retrieveProduct;
