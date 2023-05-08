fc7f77fc915d038648e95c8c1cc9eda8ee28684c
Shreyash Devendra Wadmalwar <sdwadmalwar@gmail.com>
2023-05-08 13:14:14 +0530

    Made changes in app.js. . added async and await to code.

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const categoriesList = document.getElementById("categories_list");
-const baseUrl = `https://www.themealdb.com/api/json/v1/1/categories.php`;
+// const categoriesList = document.getElementById("categories_list");
+// const baseUrl = `https://www.themealdb.com/api/json/v1/1/categories.php`;
 
-fetch(baseUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    data.categories.forEach((category) => {
-      const listItem = `
+// fetch(baseUrl)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     data.categories.forEach((category) => {
+//       const listItem = `
+//       <div class="container_item">
+//         <div class="container_img">
+//           <img src="${category.strCategoryThumb}" alt="${category.strCategory}">
+//         </div>
+//         <div class= "container_name">
+//           <h3 class"container_name_h3">${category.strCategory}</h3>
+//           <p class="container_name_p">${category.strCategoryDescription}</p>
+//         </div>
+//         </div>
+//       `;
+//       categoriesList.insertAdjacentHTML("beforeend", listItem);
+//     });
+//   });
+
+async function fetchData() {
+  const categoriesList = document.getElementById("categories_list");
+ const baseUrl = `https://www.themealdb.com/api/json/v1/1/categories.php`;
+
+
+ try {
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+  data.categories.forEach((category)=>{
+    const listItem = `
       <div class="container_item">
         <div class="container_img">
           <img src="${category.strCategoryThumb}" alt="${category.strCategory}">
@@ -17,5 +41,11 @@ fetch(baseUrl)
         </div>
       `;
       categoriesList.insertAdjacentHTML("beforeend", listItem);
-    });
   });
+
+ }catch(error){
+  console.log(error);
+ }
+}
+
+fetchData();
